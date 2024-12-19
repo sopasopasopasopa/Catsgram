d897983a83cb5f65118a471f7942420c71ebd7a3
sopa <vnkvd571@yandex.ru>
2024-12-19 17:30:07 +0300

    First commit

---
+++ src/main/java/ru/yandex/practicum/catsgram/exception/ConditionsNotMetException.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.catsgram.exception;
+
+public class ConditionsNotMetException extends RuntimeException{
+
+    public ConditionsNotMetException(String massage) {
+        super(massage);
+    }
+}
